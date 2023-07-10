0e2e4a7eb6748b21e3e7d3475f74b09bb322bccc
Saksham-Gupta-30 <skshmgupt@gmail.com>
2023-07-11 00:30:53 +0530

    Fourth Commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true)
-                const response = await fetch('http://localhost:8080/api/v1/deepai', {
+                const response = await fetch('https://splash-h9s8.onrender.com/api/v1/deepai', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -48,7 +48,7 @@ const CreatePost = () => {
             setLoading(true)
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post',{
+                const response = await fetch('https://splash-h9s8.onrender.com/api/v1/post',{
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             setLoading(true)
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://splash-h9s8.onrender.com/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
